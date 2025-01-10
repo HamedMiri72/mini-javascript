240f9ec433f0130373573351213f5445d92fec36
HamedMiri72 <hamedmiri72@gmail.com>
2025-01-10 13:26:40 +0000

    local storage

--- notes/note.js
+++ notes/note.js
@@ -2,13 +2,38 @@ const noteContainer = document.querySelector(".notes-container");
 const createButton = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    noteContainer.innerHTML = localStorage.getItem("notes");
+}
 
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes", noteContainer.innerHTML);
+}
 
 createButton.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
-    img.src = "images/delet.png";
+    img.src = "images/delete.png";
     noteContainer.appendChild(inputBox).appendChild(img);
+    
 })
+
+noteContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if(e.target.tagName === "p"){
+        notes.document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+
+    }
+})
+
